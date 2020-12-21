931e10c2cd24491769d878290cd832c4fe6dc1d8
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2020-12-21 01:37:20 +0100

    Changing from Java to Kotlin

--- chat-backend/src/main/java/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Group;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface GroupRepository extends ReactiveCrudRepository<Group,String> {
-}
--- chat-backend/src/main/java/repository/MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package repository;
-
-import model.Message;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface MessageRepository extends ReactiveCrudRepository<Message,String> { }
