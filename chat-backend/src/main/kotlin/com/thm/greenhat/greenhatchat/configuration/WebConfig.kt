87e6e0df9753c9a887764a2acb4434a0f146ecfe
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2021-01-04 17:56:02 +0100

    cors config

--- chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/configuration/WebConfig.kt
+++ chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/configuration/WebConfig.kt
@@ -1,6 +1,8 @@
+import com.thm.greenhat.greenhatchat.repository.GroupRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsConfigurationSource
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
@@ -17,7 +19,18 @@ class WebConfig: WebFluxConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("*")
                 .allowedOrigins("http://localhost:4444")
-                .allowedMethods("POST", "GET")
+                .allowedMethods("POST", "GET","OPTIONS")
                 .allowedHeaders("*")
     }
+
+    /*
+    @Bean
+    fun corsConfiguration : CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+        corsConfig.applyPermitDefaultValues()
+        corsConfig.addAllowedMethod(HttpMethod.POST)
+        corsConfig.addAllowedMethod(HttpMethod.GET)
+        corsConfig.allowed
+
+    }*/
 }
