46b8ccd0f0e582d92d128229e985805ce1ded219
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2021-01-04 16:37:45 +0100

    trying some cors config

--- chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/configuration/WebConfig.kt
+++ chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/configuration/WebConfig.kt
@@ -1,17 +1,25 @@
-package com.thm.greenhat.greenhatchat.configuration
-
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 import org.springframework.web.reactive.config.EnableWebFlux
-import org.springframework.web.reactive.config.WebFluxConfigurer
+import java.util.*
 
-    @Configuration
-    @EnableWebFlux
-    class WebConfig: WebFluxConfigurer {
-        override fun addCorsMappings(registry: CorsRegistry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("*") // any host or put domain(s) here
-                    .allowedMethods("*") // put the http verbs you want allow
-                    .allowedHeaders("*") // put the http headers you want allow
-        }
+
+@EnableWebFluxSecurity
+@EnableWebFlux
+@Configuration
+class WebConfig {
+    @Bean
+    fun springFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+       return http.cors().and().csrf().disable().build()
     }
+}
