4c7988aa275ba1dc34f3eb9ab1906c3c471d7ddc
Yasin Eraslan <yasin.eraslan@mni.thm.de>
2021-01-13 08:58:09 +0100

    Got a working websocket

--- chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/controller/KafkaConsumerController.kt
+++ chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/controller/KafkaConsumerController.kt
@@ -1,21 +1,34 @@
 package com.thm.greenhat.greenhatchat.controller
 
 import com.thm.greenhat.greenhatchat.model.Message
+import org.springframework.context.annotation.Bean
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
+import org.springframework.web.reactive.socket.WebSocketHandler
+import reactor.core.publisher.Sinks
+
 
 @Controller
 class KafkaConsumerController{
 
+    val sink = Sinks.many().multicast().onBackpressureBuffer<String>()
+
+
     @KafkaListener(topics = ["mytopic"], groupId = "test-consumer-group")
-    fun receiveData(message:Message) :Message{
-        return sendToSocket(message)
+    fun receiveData(message: Message) {
+        this.sink.tryEmitNext(message.content)
     }
 
-    @SendTo("/notify")
-    fun sendToSocket(message: Message) : Message {
-        return message
+    @Bean
+    fun handlerMapping(): HandlerMapping {
+        val map = mapOf("/fromKafka" to
+                WebSocketHandler { session ->
+                    session.send(this.sink.asFlux().map(session::textMessage))
+                })
+        return SimpleUrlHandlerMapping(map, -1)
     }
+
+
 }
