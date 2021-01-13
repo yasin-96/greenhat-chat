1aeb2da667c5c04c49d0e1f68efaa1467a4cc904
Alexander Jajzler <alexander.jajzler@mni.thm.de>
2021-01-13 19:32:13 +0100

    backend: Add missing dependency and add some parts to convert class to string

--- chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/controller/KafkaConsumerController.kt
+++ chat-backend/src/main/kotlin/com/thm/greenhat/greenhatchat/controller/KafkaConsumerController.kt
@@ -1,9 +1,8 @@
 package com.thm.greenhat.greenhatchat.controller
 
 import com.thm.greenhat.greenhatchat.model.Message
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Controller
@@ -12,34 +11,27 @@ import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.WebSocketHandler
 import reactor.core.publisher.Sinks
 
-
-@Serializable
-data class WebSocketRequest(
-       val text:String
-)
-
 @Controller
-class KafkaConsumerController{
-    val format = Json { encodeDefaults = true }
+class KafkaConsumerController {
 
     val sink = Sinks.many().multicast().onBackpressureBuffer<String>()
-
-    @KafkaListener(topics = ["mytopic"], groupId = "test-consumer-group")
-    fun receiveData(message: Message) {
-        this.sink.tryEmitNext(message.content)
-    }
 
     @Bean
     fun handlerMapping(): HandlerMapping {
         val map = mapOf("/fromKafka" to
-                WebSocketHandler { session->
+                WebSocketHandler { session ->
 
-                    println(session)
-                    session.send(this.sink.asFlux().map{
-                        val output = format.encodeToString(WebSocketRequest("green"))
-                        session.textMessage(format.encodeToString(output))
+                    session.send(this.sink.asFlux().map {
+                        println("Session: ${session.id}, \t Message> ${it}")
+                        session.textMessage(it);
                     })
                 })
         return SimpleUrlHandlerMapping(map, -1)
+    }
+
+    @KafkaListener(topics = ["mytopic"], groupId = "test-consumer-group")
+    fun receiveData(message: Message) {
+        val jsonObject = Json.encodeToString<Message>(message);
+        this.sink.tryEmitNext(jsonObject)
     }
 }
