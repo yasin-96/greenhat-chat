cf2810d5c7f1fe6ecb9da876641b449187588830
Yusuf Khan <yusuf.khan@mni.thm.de>
2021-01-22 19:10:03 +0100

    git

--- gitlab-runner/gitlab-runner.sh
+++ gitlab-runner/gitlab-runner.sh
@@ -2,9 +2,15 @@
 
 usermod -aG docker gitlab-runner
 
-docker exec -it gitlab-runner gitlab-runner register \
+registration_token=LmuyK5zd1QFfcPfuKWms
+
+docker exec -it gitlab-runner register \
   --non-interactive \
-  --registration-token LmuyK5zd1QFfcPfuKWms \
-  --description shell-runner \
-  --url http://10.48.19.45 \
-  --executor shell
+  --registration-token ${registration_token} \
+  --locked=false \
+  --description docker-runner \
+  --url http://10.48.19.45:80 \
+  --executor docker \
+  --docker-image alpine:latest \
+  --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+  --docker-network-mode gitlab
