ee3fa2402129a916bc2b47124f261bee89f31ddd
Alexander Jajzler <alexander.jajzler@mni.thm.de>
2021-02-07 15:51:06 +0100

    frontend: On incoming message scroll to botton #49

--- chat-frontend/src/components/Chat/MainContent/ChatMessages.vue
+++ chat-frontend/src/components/Chat/MainContent/ChatMessages.vue
@@ -1,15 +1,20 @@
 <template>
-  <v-container fluid class="overflow-y-auto mb-72">
+  <v-container id="chatMessages" fluid class="overflow-y-auto mb-72">
     <v-timeline v-if="messagesToDisplay">
-      <v-timeline-item v-for="(msg, index) in messagesToDisplay.messages" :key="index" large color="transparent">
+      <v-timeline-item
+        v-for="(msg, index) in messagesToDisplay.messages"
+        :key="index"
+        large
+        color="transparent"
+      >
         <template v-slot:icon>
           <v-tooltip bottom>
             <template v-slot:activator="{ on, attrs }">
-              <v-avatar  v-on="on" v-bind="attrs" v-if="msg.user.avatarPicture" class="tile mx-auto rounded" size="36">
+              <v-avatar v-on="on" v-bind="attrs" v-if="msg.user.avatarPicture" class="tile mx-auto rounded" size="36">
                 <img :src="msg.user.avatarPicture" alt="User" />
               </v-avatar>
 
-              <span  v-on="on" v-bind="attrs" v-else class="title">{{ msg.user.avatarName }}</span>
+              <span v-on="on" v-bind="attrs" v-else class="title">{{ msg.user.avatarName }}</span>
             </template>
             <span>{{ msg ? msg.user.username : '' }}</span>
           </v-tooltip>
@@ -29,22 +34,41 @@
 
 <script>
 import { mapState } from 'vuex';
+
+/**
+ *
+ */
 export default {
   name: 'ChatMessages',
-  data: () => ({
-  }),
+  updated() {
+    console.log("SCrolle jetzt", this.sizeOfMessages)
+     this.$vuetify.goTo(this.pageHeight)
+    },
   computed: {
     ...mapState({
-      activeGroupId:  (state) => state.group.activeGroupId,
+      activeGroupId: (state) => state.group.activeGroupId,
       activeGroup: (state) => state.group.activeGroup,
       messagesFromGroup: (state) => state.chat.messagesOfGroup,
     }),
-    messagesToDisplay(){
-      if(this.messagesFromGroup && !!this.messagesFromGroup && this.messagesFromGroup.length && this.activeGroupId){
-        return this.messagesFromGroup.find((msg) => msg.gid = this.activeGroupId)
+    /**
+     *
+     */
+    messagesToDisplay() {
+      if (this.messagesFromGroup && !!this.messagesFromGroup && this.messagesFromGroup.length && this.activeGroupId) {
+        return this.messagesFromGroup.find((msg) => (msg.gid = this.activeGroupId));
       }
-      return null
-    }
+      return null;
+    },
+
+    sizeOfMessages() {
+      if (this.messagesToDisplay) {
+        return this.messagesToDisplay.messages.length;
+      }
+      return null;
+    },
+    pageHeight() {
+      return document.getElementById("chatMessages").scrollHeight;
+    },
   },
 };
 </script>
